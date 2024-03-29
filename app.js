ad6c81a0bc859e074c13f62e0bc4d6b755970323
Abdul Moiz Sabro <bravia.mj@gmail.com>
2024-03-30 03:35:32 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+
+// function num() {
+//     var rand = Math.floor(Math.random() * 9)
+//     alert(rand)
+// }
+
+// num()
+function num1(){
+    var rand = Math.floor(Math.random() * 9)
+alert(rand)
+// var num = ""
+// for (i = 1; i <= 10;i++) {
+//     alert(num + i)
+}
+// var rand = Math.floor(Math.random() * 9)
+// document.write(rand)
